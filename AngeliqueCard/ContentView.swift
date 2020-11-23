61f86204769594be7e7e832b9fac1c53715153e8
jessangel79 <babin.angelique@gmail.com>
2020-11-23 19:28:14 +0100

    add lauchscreen and icon app

--- AngeliqueCard/ContentView.swift
+++ AngeliqueCard/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color("Card Color")
+//            Color(UIColor(red: 0.38, green: 0.64, blue: 0.74, alpha: 1.00))
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("angelique")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4.0))
+                Text("Angélique Babin")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+33 6 67 41 28 62‬", imageName: "phone.fill")
+                InfoView(text: "babin.angelique@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 11")
     }
 }
+
+
---
+++ AngeliqueCard/InfoView.swift
@@ -0,0 +1,35 @@
+//
+//  InfoView.swift
+//  AngeliqueCard
+//
+//  Created by Angelique Babin on 23/11/2020.
+//
+
+import SwiftUI
+
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50.0)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.blue)
+                Text(text)
+                    .foregroundColor(Color("Info Color"))
+            })
+            .padding(.all)
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
